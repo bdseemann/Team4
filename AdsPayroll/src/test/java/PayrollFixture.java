fc6362aa8d804f2448a3ac64e7a1105e5056be9b
Don Drozd <ddrozd@routeone.com>
2018-07-18 13:57:55 -0400

    added asserts for payrollFixture

--- AdsPayroll/src/test/java/PayrollFixture.java
+++ AdsPayroll/src/test/java/PayrollFixture.java
@@ -1,5 +1,30 @@
 public class PayrollFixture {
+    private Employee employee;
+
     public boolean pay() {
         return PayrollSystem.getInstance().pay();
+    }
+
+    public void setId(String employeeNumber) {
+        employee = PayrollSystem.getInstance().getEmployee(employeeNumber);
+    }
+
+    public String name() {
+        return employee.getName();
+    }
+    public String base() {
+        return employee.getBasePay().toCleanString();
+    }
+
+    public String gross() {
+        return employee.getGrossPay().toCleanString();
+    }
+
+    public String tax() {
+        return employee.getFederalIncomeTax().toCleanString();
+    }
+
+    public String net() {
+        return employee.getNetPay().toCleanString();
     }
 }

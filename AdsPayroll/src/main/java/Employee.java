0c85b99278d527fdea0111b9c49e6299cdfa0be7
Andrew Deweese <adeweese@routeone.com>
2018-07-19 11:10:54 -0400

    loops are fun

--- AdsPayroll/src/main/java/Employee.java
+++ AdsPayroll/src/main/java/Employee.java
@@ -76,7 +76,7 @@ public class Employee {
 	}
 
 	public EmployeeAccount getLastPaidAccount() {
-		for (int i = checks.size() - 1; i == 0; i--) {
+		for (int i = checks.size() - 1; i >= 0; i--) {
 			if (checks.get(i).isPaid()) {
 				return checks.get(i);
 			}
